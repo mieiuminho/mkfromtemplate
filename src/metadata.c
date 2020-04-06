c9aea20f4dd5b8bb5200ccbcd8e8897559169d6e
Nelson Estevão <nelsonmestevao@gmail.com>
2020-04-06 12:47:02 +0100

    Remove comments

--- src/metadata.c
+++ src/metadata.c
@@ -10,14 +10,9 @@
 
 #include "util.h"
 
-// static char *filtered_template_path;
-// static FILE *filtered_template;
 static GHashTable *hash_table_metadata;
 
 void metadata_init(char *project_name) {
-    // filtered_template_path = "/tmp/.filtered_template";
-    // filtered_template = fopen(filtered_template_path, "w");
-
     if (hash_table_metadata == NULL) {
         hash_table_metadata = g_hash_table_new(g_str_hash, g_str_equal);
     }
@@ -45,6 +40,3 @@ void metadata_replace_values_in_template(const char *template) {
                          (GHFunc)metadata_replace_value_in_template,
                          (gpointer) template);
 }
-
-// char *metadata_get_filtered_template_path() { return filtered_template_path;
-// }
